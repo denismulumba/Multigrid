eb09803231dfa37f1fa69c3225dc53b8b332833c
Denis Mulumba <denismulumba@grad-phdcomp-03.boisestate.edu>
2023-04-11 09:33:08 -0600

    Jacobi smoother

--- jacobi.m
+++ jacobi.m
@@ -8,10 +8,11 @@ function x = jacobi(A,b,x0,steps,omega)
 %          steps  number of iteration steps
 %          omega  relaxation parameter
 % OUTPUT:  x      approximate solution
-
-D = diag(A);
-inv_D = 1./D;
-x = x0;
-for k = 1:steps
-    x = x + omega*inv_D.*(b - A*x);
+    
+    D = diag(A);
+    inv_D = 1./D;
+    x = x0;
+    for k = 1:steps
+        x = x + omega*inv_D.*(b - A*x);
+    end
 end
