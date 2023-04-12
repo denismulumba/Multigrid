7cb7f6897f0f6557c8eee9b567a5014e4bdfffa4
Denis Mulumba <denismulumba@grad-phdcomp-03.boisestate.edu>
2023-04-11 20:45:27 -0600

    Restriction

---
+++ restriction.m
@@ -0,0 +1,29 @@
+
+
+function r = restriction(u)
+% Restrict a 2D array of grid values to a coarser grid
+%using full-weighting restriction
+    
+    [m,n] = size(u);
+    m_coarse = floor((m-1)/2)+1;
+    n_coarse = floor((n-1)/2)+1;
+    r = zeros(m_coarse, n_coarse);
+    
+    % loop over coarse grid points
+    
+    for i = 1:m_coarse
+        for j = 1:n_coarse
+            % determine indices of neigbouring fine grid points
+    
+            i1 = 2*i - 1;
+            i2 = 2*i;
+            j1 = 2*j -1;
+            j2 = 2*j;
+    
+            %compute the restricted value
+            r(i,j) = 0.24*(u(i1,j1)+u(i1,j2)+u(i2,j1)+u(i2,j2));
+    
+        end
+    end
+end
+
